c06b690d5ae7ca63cd5f172fe00367fd91d1710e
Aries <liyuntao0321@gmail.com>
2024-04-24 22:54:39 +0800

    'feat:editor'

--- app/(main)/blog/edit/page.tsx
+++ app/(main)/blog/edit/page.tsx
@@ -1,29 +1,40 @@
-'use client'
-import Quill from 'quill'
-import { useEffect } from 'react'
+"use client";
+import Quill from "quill";
+import { Delta } from "quill/core";
+import { useEffect, useState } from "react";
 
 const BlogEditPage = () => {
+  const [quillIns, setQuillIns] = useState<InstanceType<typeof Quill> | null>(
+    null
+  );
+
   useEffect(() => {
-    const quill = new Quill('#editor', {
-      debug: 'info',
-      modules: {
-        toolbar: true
-      },
-      theme: 'snow'
-    })
-  }, [])
+    setQuillIns(
+      new Quill("#editor", {
+        //   debug: "info",
+        modules: {
+          toolbar: true,
+        },
+        theme: "snow",
+      })
+    );
+    return () => {
+      setQuillIns(null);
+      const wrapper = document.querySelector("#editor-wrapper");
+      if (!wrapper) return;
+      console.log(wrapper.querySelector('div[role="toolbar"]'));
+      wrapper.querySelector('div[role="toolbar"]')?.remove();
+      const dom = document.querySelector("#editor");
+      if (!dom) return;
+      dom.innerHTML = "";
+    };
+  }, []);
 
   return (
-    <div className="w-full">
-      <div id="editor-toolbar">
-        <span className="ql-formats">
-          <button className="ql-bold"></button>
-        </span>
-      </div>
+    <div id="editor-wrapper" className="w-full">
       <div id="editor">edit</div>
-      <div id="editor1">edit</div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogEditPage
+export default BlogEditPage;
